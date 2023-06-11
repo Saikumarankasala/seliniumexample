3d867a573bc62df8e86a744ca0af032c72a1d9d2
Saikumarankasala <saiankasala502@gmail.com>
2023-06-11 09:21:20 +0530

    second commit

--- src/launchbrowser.java
+++ src/launchbrowser.java
@@ -1,9 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class launchbrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver","D://chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://icrtc.com/");
+		driver.manage().window().maximize();
 	}
 
 }
